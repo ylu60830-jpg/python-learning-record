0ca6deedf1fd0a463aab2abae9fde40f5ff0a5b8
熠枫 <2112209451@qq.com>
2026-07-10 17:18:29 +0800

    整理: 统一按日期目录结构，补全07-06至07-10全部练习

---
+++ 2026-07-09/26-7-9(2).py
@@ -0,0 +1,9 @@
+students = [
+    {"名字":"张三", "年龄":20},
+    {"名字":"李四", "年龄":21},
+    {"名字":"王五", "年龄":19}
+]
+print(students[0])
+print(students[0]["年龄"])
+print(students[0]["名字"])
+print(students)
